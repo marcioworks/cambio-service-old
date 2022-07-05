1e4199c0a9b681685642e0150b292250036631eb
Marcio Santos <marcioadsworks@gmail.com>
2022-07-05 20:48:09 -0300

    first commit

---
+++ src/main/java/br/com/marcio/cambioservice/model/Cambio.java
@@ -0,0 +1,89 @@
+package br.com.marcio.cambioservice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Cambio implements Serializable {
+
+    private static final long serialVersionUID= 1L;
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String enviroment;
+
+    public Cambio(){}
+
+    public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String enviroment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.enviroment = enviroment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public BigDecimal getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(BigDecimal convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnviroment() {
+        return enviroment;
+    }
+
+    public void setEnviroment(String enviroment) {
+        this.enviroment = enviroment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cambio cambio = (Cambio) o;
+
+        return id != null ? id.equals(cambio.id) : cambio.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
